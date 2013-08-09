506f3eb4c800adfb3b6af81c9664d5f803e2e23c
theminotaur <mxpen26@gmail.com>
2013-08-09 11:28:32 +0100

    comments!
    +fixed log.php

--- getSights.php
+++ getSights.php
@@ -3,11 +3,15 @@
 	$lng = $_GET["lng"];
 	$postcode = $_GET["postcode"];
 
+	//writes to txt file read by python
+
 	$file = fopen("APIfile.json","w",);
-	fwrite($file, '["'.$postcode.'""]');
+	fwrite($file, json_encode('["'.$postcode.'""]');
 	fclose($file);
 
 	exec("python get_where_can_I_find.py");
+
+	//returns json edited by python
 
 	$wFile = file_get_contents("APIfile.json");
 	echo $wFile;
